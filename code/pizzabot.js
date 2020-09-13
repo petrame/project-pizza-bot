6448b48d39921f28cd3fe17812f862d07cea2b76
petrame <63057558+petrame@users.noreply.github.com>
2020-09-13 23:43:45 +0200

    Update pizzabot.js

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,60 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const receiveData = (event) => {
+    event.preventDefault();
 
-const pizzaPrice = 80
+    const typeOfPizza = validateOrderName();
+    const orderQuantity = document.getElementById("quantity").value;
+    const totalCost = calculateTotalCost(orderQuantity);
+    const cookingTime = calculateCookingTime(orderQuantity);
+    document.getElementById("message").innerHTML = (`You've ordered ${orderQuantity} ${typeOfPizza}. It will cost ${totalCost} kr and take ${cookingTime} minutes. Enjoy your meal!`);
+    showImage(typeOfPizza);
+}
 
-//Put your Javscript code here:
+let orderName;
+const validateOrderName = () => {
+
+    let pizzaChoices = document.getElementsByName('pizza');
+
+    for (let i = 0; i < pizzaChoices.length; i++) {
+        if (pizzaChoices[i].checked) {
+            return pizzaChoices[i].value;
+        }
+    }
+}
+
+///Calculate total cost of the order.
+const calculateTotalCost = (orderQuantity) => {
+    const pizzaPrice = 80;
+    return orderQuantity * pizzaPrice;
+}
+
+
+//Calculate cooking time
+const calculateCookingTime = (orderQuantity) => {
+    let timeCooking;
+    if (orderQuantity < 3) {
+        timeCooking = 10;
+    } else if (orderQuantity > 5) {
+        timeCooking = 20;
+    } else {
+        timeCooking = 15;
+    }
+    return timeCooking;
+}
+
+const showImage = (typeOfPizza) => {
+
+    document.getElementById("vegetarianImg").style.display = "none";
+    document.getElementById("hawaiianImg").style.display = "none";
+    document.getElementById("pepperoniImg").style.display = "none";
+    document.getElementById("chickenImg").style.display = "none";
+
+    if (typeOfPizza === "Vegetarian Pizza") {
+        document.getElementById("vegetarianImg").style.display = "block";
+    } else if (typeOfPizza === "Hawaiian Pizza") {
+        document.getElementById("hawaiianImg").style.display = "block";
+    } else if (typeOfPizza === "Pepperoni Pizza") {
+        document.getElementById("pepperoniImg").style.display = "block";
+    } else {
+        document.getElementById("chickenImg").style.display = "block";
+    }
+}
